02b1327be0e7a3a542dbb8953d37415afe1377cb
Ndefo Afam <ndefoafam@gmail.com>
2023-11-03 19:18:39 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,58 +4,36 @@
 
 import sys
 
-def is_safe(board, row, col):
-    # Check if a queen can be placed at board[row][col]
-    
-    # Check the column
-    for i in range(row):
-        if board[i][col] == 1:
-            return False
-    
-    # Check upper left diagonal
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    
-    # Check upper right diagonal
-    for i, j in zip(range(row, -1, -1), range(col, len(board))):
-        if board[i][j] == 1:
-            return False
-    
-    return True
-
-def solve_nqueens(board, row):
-    # Recursively solve the N Queens problem
-    if row == len(board):
-        # All queens are placed, print the solution
-        for r in board:
-            print(r)
-        print()
-        return
-    
-    for col in range(len(board)):
-        if is_safe(board, row, col):
-            board[row][col] = 1
-            solve_nqueens(board, row + 1)
-            board[row][col] = 0
-
-def nqueens(N):
-    if not N.isdigit():
+def nqueens(n):
+    if not isinstance(n, int):
         print("N must be a number")
         sys.exit(1)
-    
-    N = int(N)
-    
-    if N < 4:
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
-    board = [[0 for _ in range(N)] for _ in range(N)]
-    solve_nqueens(board, 0)
+    def is_safe(board, row, col):
+        for r, c in board:
+            if r == row or c == col or r + c == row + col or r - c == row - col:
+                return False
+        return True
+    def backtrack(board, row):
+        if row == n:
+            print(board)
+            return
+        for col in range(n):
+            if is_safe(board, row, col):
+                board.append([row, col])
+                backtrack(board, row + 1)
+                board.pop()
+    backtrack([], 0)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
-    nqueens(sys.argv[1])
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    nqueens(n)
